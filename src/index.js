147f19ab410c67e9f9557c839c48d7673f4de35d
ArthurWosniaki <arthur.wosniaki.acorns@gmail.com>
2025-10-06 09:36:06 -0300

    Fixing error + removing unused packages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const workerUrl = new URL('/sw.js', location.origin);
+const workerUrl = new URL('/sw.js', window.location.origin);
 console.log({ workerUrl });
 
 // In your application code, set the "enableApiMocking" search parameter
 // on the worker script URL. This way the client lets the worker know
 // whether it should import MSW's worker script based on the environment.
+console.log({ nodeEnv: process.env.NODE_ENV });
 if (process.env.NODE_ENV === 'development') {
   workerUrl.searchParams.set('enableApiMocking', 'true');
 }
@@ -16,11 +17,13 @@ if (process.env.NODE_ENV === 'development') {
 navigator.serviceWorker.register(workerUrl.href);
 
 async function enableMocking() {
+  console.log({ nodeEnv: process.env.NODE_ENV });
   if (process.env.NODE_ENV !== 'development') {
     return;
   }
 
   const { worker } = await import('./mocks/browser');
+  console.log({ worker });
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
